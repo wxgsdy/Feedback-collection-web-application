b5b2295add0404c4abb98e08e0a221a884e8d5ea
wxgsdy <wxgsdy@gmail.com>
2018-08-30 08:07:12 -0700

    finished heroku and start Passport JS

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
 
 
-app.get('/', (req, res) => {
-	res.send({hi:'there'});
-});
-
-
+passport.use(
+	new GoogleStrategy({
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+	}, (accessToken) => {
+		console.log(accessToken);
+	})
+);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
