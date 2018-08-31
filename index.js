2bca26a32ad12b4139f4d1fab7c822b1a8aa6de3
wxgsdy <wxgsdy@gmail.com>
2018-08-31 01:35:37 -0700

    Finished Google OAuth configuration

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const keys = require('./config/keys');
 
 
 passport.use(
-	new GoogleStrategy({
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
-	}, (accessToken) => {
-		console.log(accessToken);
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		}, 
+		(accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refresh token', refreshToken);
+			console.log('profile:::::', profile);
+		}
+	)
+);
+
+app.get(
+	'/auth/google', 
+	passport.authenticate('google',{
+		scope: ['profile', 'email']
 	})
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
