d22519a921411abcd92424a099705a945b15e59c
wxgsdy <wxgsdy@gmail.com>
2018-09-05 02:29:18 -0700

    added Redux and Redux-trunk to communicate React and Node

--- services/passport.js
+++ services/passport.js
@@ -25,17 +25,15 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		}, 
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({googleId: profile.id}).then((existingUser) =>{
-				if(existingUser){
-			  		// we calready heve one user for this googleId
-			  		done(null, existingUser);			  		
-			  	} else {
-			    	new User({googleId: profile.id})
-			    	    .save()
-			    		.then(user => done(null, user));
-			  	}
-			});			
+
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({googleId: profile.id})
+			
+			if(existingUser){		  		
+		  		return done(null, existingUser);			  		
+		  	} 	  		
+	    	const user = await new User({googleId: profile.id}).save()
+	    	done(null, user);		  					
 		}
 	)
 );
