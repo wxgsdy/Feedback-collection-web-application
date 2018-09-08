5fb3996306108a733af0488a70a75bc4f7f4aee9
wxgsdy <wxgsdy@gmail.com>
2018-09-08 02:40:05 -0700

    finished buiding payment system with Stripe

--- client/src/index.js
+++ client/src/index.js
@@ -17,3 +17,6 @@ ReactDOM.render(
 	<Provider store={store}><App /></Provider>,
 	document.querySelector('#root')
 );
+
+console.log('stripe key is', process.env.REACT_APP_STRIPE_KEY);
+console.log('enviroment is', process.env.NODE_ENV);
