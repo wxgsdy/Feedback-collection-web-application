1ab8a58ebf28f670b1a330ed1938ae8c16779fc0
wxgsdy <wxgsdy@gmail.com>
2018-09-09 23:47:40 -0700

    finished back end Mailer and combined SendGrid

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,10 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// for http test
+import axios from 'axios';
+window.axios = axios; 
+
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 
@@ -25,6 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
 	// express will serve up production assests  like our main.js file, or main.css file 
