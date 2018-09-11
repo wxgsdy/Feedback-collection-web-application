5c3b87660608d28cda1edfaac5dc492e7796a244
wxgsdy <wxgsdy@gmail.com>
2018-09-11 01:03:37 -0700

    finished front-end survey creating related pages with Redux form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,9 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-const DashBoard = () => <h2>DashBoard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
-
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component{
 
@@ -19,10 +18,10 @@ class App extends Component{
 		return(
 			<div className="container">
 				<BrowserRouter>
-					<div>
+					<div className="container">
 						<Header />
 						<Route exact path="/" component = {Landing} />
-						<Route exact path="/surveys" component={DashBoard} />
+						<Route exact path="/surveys" component={Dashboard} />
 						<Route path="/surveys/new" component={SurveyNew} />
 					</div>
 				</BrowserRouter>
